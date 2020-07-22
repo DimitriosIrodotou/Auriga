6f59f92a7b55466561c78118a33fbf334aee6046
DimitriosIrodotou <di.irodotou@gmail.com>
2020-07-22 14:46:42 +0100

    Added a Sersic+exponential fit, started fitting Au-18

--- main_scripts/analyse_fits_images.py
+++ main_scripts/analyse_fits_images.py
@@ -3,6 +3,7 @@ import os
 import PIL
 import time
 import glob
+import plot_tools
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -29,30 +30,30 @@ def convert_for_fit(name, min_intensity):
     # Load a png image and convert to 512x512 #
     image_png = Image.open(name + '.png').convert("L")
     image_png = image_png.resize((512, 512), PIL.Image.NEAREST)
-    
+
     # Create and save a fits version of the gray scaled image #
     hdu = fits.PrimaryHDU(image_png)
     hdu.writeto(name + '_ctf.fits', overwrite=True)
-    
+
     # Add flat background noise, with specified local variance at each point #
     image_fits = fits.getdata(name + '_ctf.fits', ext=0)
     image_fits = image_fits + min_intensity
-    
+
     # Add Gaussian blur #
     FWHM = 3
     sigma = FWHM / np.sqrt(8 * np.log(2))
     image_fits = gaussian_filter(image_fits, sigma=sigma)
     array = np.asarray(image_fits)
-    
+
     # Generate the figure and set its parameters #
     figure, axis = plt.subplots(1, figsize=(10, 10), frameon=False)
     plt.axis('off')
     axis.set_aspect('equal')
-    
+
     # Create and save a gray scaled version of the image #
     plt.imsave(name + '_ctf.png', array, cmap='gray')
     plt.close()
-    
+
     # Create and save a fits version of the image #
     hdu = fits.PrimaryHDU(array)
     hdu.writeto(name + '_ctf.fits', overwrite=True)
@@ -69,16 +70,17 @@ def fit_isophotal_ellipses(name, ellipticity):
     # Load the fits image and calculate the centre #
     image_fits = fits.getdata(name + '_ctf.fits', ext=0)
     centre = get_image_centre(name)
-    
+
     # Provide the elliptical isophote fitter with an initial ellipse (geometry) and fit multiple isophotes to the image array #
     geometry = EllipseGeometry(x0=centre[0], y0=centre[1], sma=centre[0] / 10, eps=ellipticity, pa=1e-2)
     ellipse = Ellipse(image_fits, geometry)
     isolist = ellipse.fit_image(minsma=1, maxsma=centre[0], step=0.3)
     print(isolist.to_table())  # Print the isophote values as a table sorted by the semi-major axis length.
-    
+    print('average ellipticity:', np.mean(isolist.eps))
+
     # Plot the ellipticity, position angle, and the center x and y position as a function of the semi-major axis length.
     # Generate the figure and set its parameters #
-    plt.figure(figsize=(10, 7.5))
+    plt.figure(figsize=(20, 15))
     gs = gridspec.GridSpec(2, 3, hspace=0.35, wspace=0.35)
     axis00 = plt.subplot(gs[0, 0])
     axis01 = plt.subplot(gs[0, 1])
@@ -86,7 +88,7 @@ def fit_isophotal_ellipses(name, ellipticity):
     axis10 = plt.subplot(gs[1, 0])
     axis11 = plt.subplot(gs[1, 1])
     axis12 = plt.subplot(gs[1, 2])
-    
+
     axes = [axis00, axis01, axis02, axis10, axis11, axis12]
     y_labels = [r'$\mathrm{Ellipticity}$', r'$\mathrm{PA\;[\deg]}$', r'$\mathrm{Pixels\;inside\;each\;ellipse}$', r'$\mathrm{x_{0}\;[pix]}$',
                 r'$\mathrm{y_{0}\;[pix]}$', r'$\mathrm{Mean\,intensity}$']
@@ -95,25 +97,34 @@ def fit_isophotal_ellipses(name, ellipticity):
               (0.9 * min(isolist.intens), 1.1 * max(isolist.intens))]
     y_errors = [isolist.ellip_err, isolist.pa_err / np.pi * 180., np.zeros(len(isolist.x0_err)), isolist.x0_err, isolist.y0_err, isolist.int_err]
     for axis, y_label, y_value, y_error, y_lim in zip(axes, y_labels, y_values, y_errors, y_lims):
-        axis.set_ylim(y_lim)
-        axis.set_xscale('log')
-        axis.set_ylabel(y_label)
-        axis.set_xlim(1e0, centre[0])
-        axis.grid(True, color='gray', linestyle='-')
-        axis.set_xlabel(r'$\mathrm{Semi-major\;axis\;length\;[pix]}$')
         axis.errorbar(isolist.sma, y_value, yerr=y_error, fmt='o', markersize=3)
-    
+        plot_tools.set_axis(axis, xlim=[1e0, centre[0]], ylim=y_lim, xscale='log', xlabel=r'$\mathrm{Semi-major\;axis\;length\;[pix]}$',
+            ylabel=y_label, which='major', aspect=None)
     axis02.set_yscale('log')
+
+    # Fit a Sersic plus exponential profile #
+    popt, pcov = curve_fit(fit_total_profile, isolist.sma, isolist.intens,
+        p0=[isolist.intens[0], 2, isolist.intens[0], 2, 4])  # p0 = [I_0d, R_d, I_0b, b, n]
+    I_0d, R_d, I_0b, b, n = popt[0], popt[1], popt[2], popt[3], popt[4]
+    axis12.plot(isolist.sma, fit_total_profile(isolist.sma, popt[0], popt[1], popt[2], popt[3], popt[4]), color='purple')
+    R_eff = b * sersic_b_n(n) ** n
+    axis12.axvline(x=R_d, color='red')
+    axis12.axvline(x=R_eff, color='blue')
+    print('I_0d:', I_0d, 'h:', R_d, 'I_0b:', I_0b, 'n:', n, 'R_eff:', R_eff)
+
+    # Fit an exponential profile #
     popt, pcov = curve_fit(fit_exponential_profile, isolist.sma, isolist.intens, p0=[isolist.intens[0], 1])
-    print('I_0:', popt[0], 'h:', popt[1])
-    axis12.plot(isolist.sma, fit_exponential_profile(isolist.sma, popt[0], popt[1]), 'b-')
-    axis12.axvline(x=popt[1])
+    axis12.plot(isolist.sma, fit_exponential_profile(isolist.sma, popt[0], popt[1]), color='black')
+    I_0d, R_d = popt[0], popt[1]
+    axis12.axvline(x=R_d, color='black')
+    print('I_0d:', I_0d, 'h:', R_d)
+
     plt.savefig(name + '_isolist_' + str(date) + '.png', bbox_inches='tight')  # Save the figure.
-    
+
     # Build an elliptical model #
     # model_image = build_ellipse_model(image_fits.shape, isolist)
     # residual = image_fits - model_image
-    
+
     # Plot the original data with some of the isophotes, the elliptical model image, and the residual image #
     # Generate the figure and set its parameters #
     plt.figure(figsize=(10, 10))
@@ -122,7 +133,7 @@ def fit_isophotal_ellipses(name, ellipticity):
     axis01 = plt.subplot(gs[0, 1])
     # axis10 = plt.subplot(gs[1, 0])
     # axis11 = plt.subplot(gs[1, 1])
-    
+
     axes = [axis00, axis01]  # , axis10, axis11]
     titles = [r'$\mathrm{Auriga-}$' + name, r'$\mathrm{Sample\;of\;isophotes}$']  # , r'$\mathrm{Ellipse\;model}$', r'$\mathrm{Residual}$']
     images = [image_fits, image_fits]  # , model_image, residual]
@@ -130,15 +141,15 @@ def fit_isophotal_ellipses(name, ellipticity):
         axis.grid(True, color='gray', linestyle='-')
         axis.set_title(title)
         axis.imshow(image, origin='lower', cmap='gray')
-    
+
     smas = np.linspace(centre[0] / 10, centre[0], 10)
     for sma in smas:
         iso = isolist.get_closest(sma)
         x, y, = iso.sampled_coordinates()
         axis01.plot(x, y, color='tab:red')
-    
+
     plt.savefig(name + '_model_' + str(date) + '.png', bbox_inches='tight')  # Save the figure.
-    
+
     return None
 
 
@@ -148,9 +159,50 @@ def fit_exponential_profile(x, I_0, R_d):
     :param x: x data.
     :param I_0: Central intensity.
     :param R_d: Scale length.
-    :return: I_0 * np.exp(-r / R_d)
+    :return: I_0 * np.exp(-x / R_d)
     """
     return I_0 * np.exp(-x / R_d)
+
+
+def fit_sersic_profile(r, I_0b, b, n):
+    """
+    Calculate a Sersic profile.
+    :param r: radius.
+    :param I_0b: Spheroid central intensity.
+    :param b: Sersic b parameter
+    :param n: Sersic index
+    :return: I_0b * np.exp(-(r / b) ** (1 / n))
+    """
+    return I_0b * np.exp(-(r / b) ** (1 / n))  # b = R_eff / b_n ^ n
+
+
+def fit_total_profile(r, I_0d, R_d, I_0b, b, n):
+    """
+    Calculate a total (Sersic + exponential) profile.
+    :param r: radius.
+    :param I_0d: Disc central intensity.
+    :param R_d: Disc scale length.
+    :param I_0b: Spheroid central intensity.
+    :param b: Sersic b parameter.
+    :param n: Sersic index.
+    :return: exponential_profile(r, I_0d, R_d) + sersic_profile(r, I_0b, b, n)
+    """
+    y = fit_exponential_profile(r, I_0d, R_d) + fit_sersic_profile(r, I_0b, b, n)
+    return y
+
+
+def sersic_b_n(n):
+    """
+    Calculate the Sersic b parameter.
+    :param n: Sersic index.
+    :return: b_n
+    """
+    if n <= 0.36:
+        b_n = 0.01945 + n * (- 0.8902 + n * (10.95 + n * (- 19.67 + n * 13.43)))
+    else:
+        x = 1.0 / n
+        b_n = -1.0 / 3.0 + 2. * n + x * (4.0 / 405. + x * (46. / 25515. + x * (131. / 1148175 - x * 2194697. / 30690717750.)))
+    return b_n
 
 
 def get_image_centre(name):
@@ -161,7 +213,7 @@ def get_image_centre(name):
     """
     # Load the png image and calculate the centre #
     im = Image.open(name + '_ctf.png')
-    
+
     (X, Y) = im.size
     centre = X / 2, Y / 2
     return centre
@@ -175,12 +227,12 @@ def plot_fits_image(name):
     """
     # Load the fits image and show it #
     image_fits = fits.getdata(name + '.fits', ext=0)
-    
+
     # Generate the figure and set its parameters #
     figure, axis = plt.subplots(1, figsize=(10, 10), frameon=False)
     plt.axis('off')
     axis.set_aspect('equal')
-    
+
     # Create and save a gray scaled version of the image #
     plt.imsave(name + str(date) + '.png', image_fits, cmap='gray')
     plt.close()
@@ -194,19 +246,19 @@ def plot_fit_data(h=0.0, R_eff=0.0):
     """
     # Load the fits image and show it #
     image_fits = fits.getdata(name + '.fits', ext=0)
-    
+
     # Generate the figure and set its parameters #
     figure, axis = plt.subplots(1, figsize=(10, 10), frameon=False)
     plt.axis('off')
     axis.set_aspect('equal')
-    
+
     axis.imshow(image_fits, cmap='gray')
     centre = get_image_centre(name)
     # Plot the scale length
     for radius in [h, R_eff]:
         circle = Circle((centre[0], centre[1]), radius, color='tab:red', fill=False)
         axis.add_patch(circle)
-    
+
     # Create and save a gray scaled version of the image #
     plt.savefig(name + '_2_' + str(date) + '.png', cmap='gray', bbox_inches='tight')
     plt.close()
@@ -218,7 +270,7 @@ Imfit_path = '/Users/Bam/PycharmProjects/Auriga/Imfit/Auriga/'
 plots_path = '/Users/Bam/PycharmProjects/Auriga/plots/projections/Imfit/'
 
 # Get the names and sort them #
-names = glob.glob(plots_path + 'Au-06NoRNoQ')
+names = glob.glob(plots_path + 'Au-18')
 names = [re.split('/Imfit|/', name)[-1] for name in names]
 names.sort()
 
@@ -226,18 +278,18 @@ names.sort()
 for name in names:
     os.chdir(plots_path + name)  # Change to each halo's plots directory
     # Prepare the image and fit isophotal ellipses #
-    # min_intensity = 47.86  # {'Au-06':85.57, 'Au-06NoRNoQ':47.86}
+    # min_intensity = 36.45  # {'Au-06':85.57, 'Au-06NoRNoQ':47.86, 'Au-18':36.45}
     # convert_for_fit(name, min_intensity)
-    # ellipticity = 0.6  # {'Au-06NoRNoQ':0.6}
+    # ellipticity = 0.42  # {'Au-06NoRNoQ':0.6, 'Au-18':0.42}
     # fit_isophotal_ellipses(name, ellipticity)
-    
+
     # Use Imfit to analyse the image #
     # --bootstrap 15
     os.chdir(Imfit_path + name)  # Change to each halo's Imfit directory
     os.system(
         '../../imfit -c %s_config.dat --nm --model-errors --cashstat ../../../plots/projections/Imfit/%s/%s_ctf.fits --save-model=%s_model.fits '
         '--save-residual=%s_resid.fits --save-params=%s_bestfit.dat' % (name, name, name, name, name, name))
-    
-    # Plot the image model and residual  #
+
+    # Plot the image model and residual #
     plot_fits_image(name + '_resid')
     plot_fits_image(name + '_model')
--- main_scripts/plot_tools.py
+++ main_scripts/plot_tools.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from matplotlib import gridspec
-from scripts.gigagalaxy.util import satellite_utilities
+# from scripts.gigagalaxy.util import satellite_utilities
 
 res = 512
 boxsize = 0.06
@@ -23,14 +23,14 @@ def binned_median_1sigma(x_data, y_data, bin_type, n_bins, log=False):
             x = np.log10(x_data)
         else:
             x = x_data
-        
+
         # Declare arrays to store the data #
         n_bins = np.quantile(np.sort(x), np.linspace(0, 1, n_bins + 1))
         slow = np.zeros(len(n_bins))
         shigh = np.zeros(len(n_bins))
         median = np.zeros(len(n_bins))
         x_value = np.zeros(len(n_bins))
-        
+
         # Loop over all bins and calculate the median and 1-sigma lines #
         for i in range(len(n_bins) - 1):
             index, = np.where((x >= n_bins[i]) & (x < n_bins[i + 1]))
@@ -39,23 +39,23 @@ def binned_median_1sigma(x_data, y_data, bin_type, n_bins, log=False):
                 median[i] = np.nanmedian(y_data[index])
                 slow[i] = np.nanpercentile(y_data[index], 15.87)
                 shigh[i] = np.nanpercentile(y_data[index], 84.13)
-        
+
         return x_value, median, shigh, slow
-    
+
     elif bin_type == 'equal_width':
         if log is True:
             x = np.log10(x_data)
         else:
             x = x_data
         x_low = min(x)
-        
+
         # Declare arrays to store the data #
         bin_width = (max(x) - min(x)) / n_bins
         slow = np.zeros(n_bins)
         shigh = np.zeros(n_bins)
         median = np.zeros(n_bins)
         x_value = np.zeros(n_bins)
-        
+
         # Loop over all bins and calculate the median and 1-sigma lines #
         for i in range(n_bins):
             index, = np.where((x >= x_low) & (x < x_low + bin_width))
@@ -65,7 +65,7 @@ def binned_median_1sigma(x_data, y_data, bin_type, n_bins, log=False):
                 slow[i] = np.nanpercentile(y_data[index], 15.87)
                 shigh[i] = np.nanpercentile(y_data[index], 84.13)
             x_low += bin_width
-        
+
         return x_value, median, shigh, slow
 
 
@@ -83,12 +83,12 @@ def binned_sum(x_data, y_data, n_bins, log=False):
     else:
         x = x_data
     x_low = min(x)
-    
+
     # Declare arrays to store the data #
     bin_width = (max(x) - min(x)) / n_bins
     sum = np.zeros(n_bins)
     x_value = np.zeros(n_bins)
-    
+
     # Loop over all bins and calculate the sum line #
     for i in range(n_bins):
         index, = np.where((x >= x_low) & (x < x_low + bin_width))
@@ -96,24 +96,25 @@ def binned_sum(x_data, y_data, n_bins, log=False):
         if len(index) > 0:
             sum[i] = np.sum(y_data[index])
         x_low += bin_width
-    
+
     return x_value, sum
 
 
-def create_colorbar(axis, plot, label, orientation='vertical', size=16):
+def create_colorbar(axis, plot, label, orientation='vertical', ticks=None, size=16):
     """
     Generate a colorbar.
     :param axis: colorbar axis.
     :param plot: corresponding plot.
     :param label: colorbar label.
     :param orientation: colorbar orientation.
+    :param ticks: array of ticks.
     :param size: text size.
     :return: None
     """
-    cbar = plt.colorbar(plot, cax=axis, orientation=orientation)
+    cbar = plt.colorbar(plot, cax=axis, ticks=ticks, orientation=orientation)
     cbar.set_label(label, size=size)
     axis.tick_params(direction='out', which='both', right='on', labelsize=size)
-    
+
     if orientation == 'horizontal':
         axis.xaxis.tick_top()
         axis.xaxis.set_label_position("top")
@@ -141,32 +142,32 @@ def set_axis(axis, xlim=None, ylim=None, xscale=None, yscale=None, xlabel=None, 
         axis.set_xlim(xlim)
     if ylim:
         axis.set_ylim(ylim)
-    
+
     # Set axis labels #
     if xlabel:
         axis.set_xlabel(xlabel, size=size)
     if ylabel:
         axis.set_ylabel(ylabel, size=size)
-    
+
     # Set axis scales #
     if xscale:
         axis.set_xscale(xscale)
     if yscale:
         axis.set_yscale(yscale)
-    
+
     if not xlim and not xlabel:
         axis.set_xticks([])
         axis.set_xticklabels([])
     if not ylim and not ylabel:
         axis.set_yticks([])
         axis.set_yticklabels([])
-    
+
     # Set grid and tick parameters #
     if aspect is not None:
         axis.set_aspect('equal')
     axis.grid(True, which=which, axis='both', color='gray', linestyle='-')
     axis.tick_params(direction='out', which=which, top='on', bottom='on', left='on', right='on', labelsize=size)
-    
+
     return None
 
 
@@ -185,7 +186,7 @@ def set_axes_evolution(axis, axis2, ylim=None, yscale=None, ylabel=None, aspect=
     """
     z = np.array([5.0, 3.0, 2.0, 1.0, 0.5, 0.2, 0.0])
     times = satellite_utilities.return_lookbacktime_from_a((z + 1.0) ** (-1.0))  # In Gyr.
-    
+
     lb = []
     for v in z:
         if v >= 1.0:
@@ -195,7 +196,7 @@ def set_axes_evolution(axis, axis2, ylim=None, yscale=None, ylabel=None, aspect=
                 lb += ["%.1f" % v]
             else:
                 lb += ["%.0f" % v]
-    
+
     # Set axis limits #
     axis2.set_xticks(times)
     axis2.set_xticklabels(lb)
@@ -203,27 +204,27 @@ def set_axes_evolution(axis, axis2, ylim=None, yscale=None, ylabel=None, aspect=
         axis.set_ylim(ylim)
     axis.set_xlim(13, 0)
     axis2.set_xlim(axis.get_xlim())
-    
+
     # Set axis scales #
     if yscale:
         axis.set_yscale(yscale)
-    
+
     # Set axis labels #
     if ylabel:
         axis.set_ylabel(ylabel, size=size)
     else:
         axis.set_yticklabels([])
-    
+
     axis.set_xlabel(r'$\mathrm{t_{look}/Gyr}$', size=size)
     axis2.set_xlabel(r'$\mathrm{z}$', size=size)
-    
+
     # Set grid and tick parameters #
     if aspect is not None:
         axis.set_aspect('equal')
     axis.grid(True, which=which, axis='both', color='gray', linestyle='-')
     axis.tick_params(direction='out', which=which, top='on', bottom='on', left='on', right='on', labelsize=size)
     axis2.tick_params(direction='out', which=which, top='on', left='on', right='on', labelsize=size)
-    
+
     return None
 
 
@@ -239,14 +240,14 @@ def create_axes_combinations(res=res, boxsize=boxsize, contour=False, colorbar=F
     :param multiple: multiple
     :return: axes
     """
-    
+
     # Set the axis values #
     x = np.linspace(-0.5 * boxsize, +0.5 * boxsize, res + 1)
     y = np.linspace(-0.5 * boxsize, +0.5 * boxsize, res + 1)
     y2 = np.linspace(-0.5 * boxsize, +0.5 * boxsize, res / 2 + 1)
-    
+
     area = (boxsize / res) ** 2  # Calculate the area.
-    
+
     # Generate the panels #
     if contour is True:
         gs = gridspec.GridSpec(2, 3, hspace=0.05, wspace=0.05, height_ratios=[1, 0.5], width_ratios=[1, 1, 0.05])
@@ -255,24 +256,24 @@ def create_axes_combinations(res=res, boxsize=boxsize, contour=False, colorbar=F
         axis10 = plt.subplot(gs[1, 0])
         axis11 = plt.subplot(gs[1, 1])
         axiscbar = plt.subplot(gs[:, 2])
-        
+
         return axis00, axis01, axis10, axis11, axiscbar, x, y, y2, area
-    
+
     elif colorbar is True:
         gs = gridspec.GridSpec(2, 2, hspace=0.05, wspace=0.05, height_ratios=[1, 0.5], width_ratios=[1, 0.05])
         axis00 = plt.subplot(gs[0, 0])
         axis10 = plt.subplot(gs[1, 0])
         axiscbar = plt.subplot(gs[:, 1])
-        
+
         return axis00, axis10, axiscbar, x, y, y2, area
-    
+
     elif velocity_vectors is True:
         gs = gridspec.GridSpec(2, 1, hspace=0.05)
         axis00 = plt.subplot(gs[0, 0])
         axis10 = plt.subplot(gs[1, 0])
-        
+
         return axis00, axis10, x, y, y2, area
-    
+
     elif multiple is True:
         gs = gridspec.GridSpec(3, 6, hspace=0.0, wspace=0.05, height_ratios=[1, 0.05, 1])
         axis00 = plt.subplot(gs[0, 0])
@@ -293,10 +294,10 @@ def create_axes_combinations(res=res, boxsize=boxsize, contour=False, colorbar=F
         axis05 = plt.subplot(gs[0, 5])
         axiscbar5 = plt.subplot(gs[1, 5])
         axis25 = plt.subplot(gs[2, 5])
-        
+
         return axis00, axiscbar, axis20, axis01, axiscbar1, axis21, axis02, axiscbar2, axis22, axis03, axiscbar3, axis23, axis04, axiscbar4, \
                axis24, axis05, axiscbar5, axis25, x, y, area
-    
+
     elif multiple2 is True:
         gs = gridspec.GridSpec(6, 3, hspace=0, wspace=0)
         axis00, axis01, axis02 = plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1]), plt.subplot(gs[0, 2])
@@ -307,7 +308,7 @@ def create_axes_combinations(res=res, boxsize=boxsize, contour=False, colorbar=F
         axis50, axis51, axis52 = plt.subplot(gs[5, 0]), plt.subplot(gs[5, 1]), plt.subplot(gs[5, 2])
         return axis00, axis01, axis02, axis10, axis11, axis12, axis20, axis21, axis22, axis30, axis31, axis32, axis40, axis41, axis42, axis50, \
                axis51, axis52, x, y, y2, area
-    
+
     elif multiple3 is True:
         gs = gridspec.GridSpec(6, 4, hspace=0.1, wspace=0.1, height_ratios=[1, 0.5, 1, 0.5, 1, 0.5], width_ratios=[1, 1, 1, 0.1])
         axis00, axis01, axis02 = plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1]), plt.subplot(gs[0, 2])
@@ -319,12 +320,12 @@ def create_axes_combinations(res=res, boxsize=boxsize, contour=False, colorbar=F
         axiscbar = plt.subplot(gs[:, 3])
         return axis00, axis01, axis02, axis10, axis11, axis12, axis20, axis21, axis22, axis30, axis31, axis32, axis40, axis41, axis42, axis50, \
                axis51, axis52, axiscbar, x, y, y2, area
-    
+
     else:
         gs = gridspec.GridSpec(2, 1, hspace=0.05, height_ratios=[1, 0.5])
         axis00 = plt.subplot(gs[0, 0])
         axis10 = plt.subplot(gs[1, 0])
-        
+
         return axis00, axis10, x, y, y2, area
 
 
@@ -342,7 +343,7 @@ def rotate_bar(z, y, x):
     beta_2 = np.zeros(n_bins)
     alpha_0 = np.zeros(n_bins)
     alpha_2 = np.zeros(n_bins)
-    
+
     # Split disc in radial bins and calculate Fourier components #
     r = np.sqrt(x[:] ** 2 + y[:] ** 2)  # Radius of each particle.
     for i in range(0, n_bins):
@@ -357,7 +358,7 @@ def rotate_bar(z, y, x):
             alpha_0[i] = alpha_0[i] + 1
             alpha_2[i] = alpha_2[i] + np.cos(2 * th_i)
             beta_2[i] = beta_2[i] + np.sin(2 * th_i)
-    
+
     # Calculate bar rotation angle for each time by averaging over radii between 1 and 5 kpc #
     r_b = 5  # In kpc.
     r_s = 1  # In kpc.
@@ -368,12 +369,12 @@ def rotate_bar(z, y, x):
             k = k + 1.
             phase_in = phase_in + 0.5 * np.arctan2(beta_2[i], alpha_2[i])
     phase_in = phase_in / k
-    
+
     # Transform back -tangle to horizontal position #
     z_pos = z[:]
     y_pos = np.cos(-phase_in) * (y[:]) + np.sin(-phase_in) * (x[:])
     x_pos = np.cos(-phase_in) * (x[:]) - np.sin(-phase_in) * (y[:])
-    
+
     return z_pos / 1e3, y_pos / 1e3, x_pos / 1e3  # In kpc.
 
 
@@ -389,38 +390,38 @@ def create_axes_projections(res=res, boxsize=boxsize, contour=False, colorbar=Fa
     :param multiple: multiple
     :return: axes
     """
-    
+
     # Set the axis values #
     x = np.linspace(-0.5 * boxsize, +0.5 * boxsize, res + 1)
     y = np.linspace(-0.5 * boxsize, +0.5 * boxsize, res + 1)
     y2 = np.linspace(-0.5 * boxsize, +0.5 * boxsize, res / 2 + 1)
-    
+
     area = (boxsize / res) ** 2  # Calculate the area.
-    
+
     # Generate the panels #
     if contour is True:
         gs = gridspec.GridSpec(2, 3, hspace=0.05, wspace=0.0, height_ratios=[1, 0.5], width_ratios=[1, 1, 0.05])
         axis00, axis01 = plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1])
         axis10, axis11 = plt.subplot(gs[1, 0]), plt.subplot(gs[1, 1])
         axiscbar = plt.subplot(gs[:, 2])
-        
+
         return axis00, axis01, axis10, axis11, axiscbar, x, y, y2, area
-    
+
     elif colorbar is True:
         gs = gridspec.GridSpec(2, 2, hspace=0.05, wspace=0.0, height_ratios=[1, 0.5], width_ratios=[1, 0.05])
         axis00 = plt.subplot(gs[0, 0])
         axis10 = plt.subplot(gs[1, 0])
         axiscbar = plt.subplot(gs[:, 1])
-        
+
         return axis00, axis10, axiscbar, x, y, y2, area
-    
+
     elif velocity_vectors is True:
         gs = gridspec.GridSpec(2, 1, hspace=0.05)
         axis00 = plt.subplot(gs[0, 0])
         axis10 = plt.subplot(gs[1, 0])
-        
+
         return axis00, axis10, x, y, y2, area
-    
+
     elif multiple is True:
         gs = gridspec.GridSpec(3, 6, hspace=0.0, wspace=0.05, height_ratios=[1, 0.05, 1])
         axis00, axis01, axis02, axis03, axis04, axis05 = plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1]), plt.subplot(gs[0, 2]), plt.subplot(
@@ -429,19 +430,19 @@ def create_axes_projections(res=res, boxsize=boxsize, contour=False, colorbar=Fa
             gs[1, 3]), plt.subplot(gs[1, 4]), plt.subplot(gs[1, 5])
         axis20, axis21, axis22, axis23, axis24, axis25 = plt.subplot(gs[2, 0]), plt.subplot(gs[2, 1]), plt.subplot(gs[2, 2]), plt.subplot(
             gs[2, 3]), plt.subplot(gs[2, 4]), plt.subplot(gs[2, 5])
-        
+
         return axis00, axis10, axis20, axis01, axis11, axis21, axis02, axis12, axis22, axis03, axis13, axis23, axis04, axis14, axis24, axis05, \
                axis15, axis25, x, y, area
-    
+
     elif multiple2 is True:
         gs = gridspec.GridSpec(4, 3, hspace=0, wspace=0, height_ratios=[1, 0.5, 1, 0.5])
         axis00, axis01, axis02 = plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1]), plt.subplot(gs[0, 2])
         axis10, axis11, axis12 = plt.subplot(gs[1, 0]), plt.subplot(gs[1, 2]), plt.subplot(gs[1, 2])
         axis20, axis21, axis22 = plt.subplot(gs[2, 0]), plt.subplot(gs[2, 1]), plt.subplot(gs[2, 2])
         axis30, axis31, axis32 = plt.subplot(gs[3, 0]), plt.subplot(gs[3, 1]), plt.subplot(gs[3, 2])
-        
+
         return axis00, axis10, axis20, axis30, axis01, axis11, axis21, axis31, axis02, axis12, axis22, axis32, x, y, y2, area
-    
+
     elif multiple3 is True:
         gs = gridspec.GridSpec(4, 4, hspace=0.05, wspace=0, height_ratios=[1, 0.5, 1, 0.5], width_ratios=[1, 1, 1, 0.1])
         axis00, axis01, axis02 = plt.subplot(gs[0, 0]), plt.subplot(gs[0, 1]), plt.subplot(gs[0, 2])
@@ -449,12 +450,12 @@ def create_axes_projections(res=res, boxsize=boxsize, contour=False, colorbar=Fa
         axis20, axis21, axis22 = plt.subplot(gs[2, 0]), plt.subplot(gs[2, 1]), plt.subplot(gs[2, 2])
         axis30, axis31, axis32 = plt.subplot(gs[3, 0]), plt.subplot(gs[3, 1]), plt.subplot(gs[3, 2])
         axiscbar = plt.subplot(gs[:, 3])
-        
+
         return axis00, axis10, axis20, axis30, axis01, axis11, axis21, axis31, axis02, axis12, axis22, axis32, axiscbar, x, y, y2, area
-    
+
     else:
         gs = gridspec.GridSpec(2, 1, hspace=0.05)
         axis00 = plt.subplot(gs[0, 0])
         axis10 = plt.subplot(gs[1, 0])
-        
+
         return axis00, axis10, x, y, y2, area
